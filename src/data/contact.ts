700ea1d7bba805951783835e829fc8b053efd2e8
brobbeymessiah <brobbeymessiah5@gmail.com>
2026-07-16 16:12:38 +0000

    git add

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -5,4 +5,6 @@ export const contact = {
   email: "info@xapnghanaltd.com",
   emailHref: "mailto:info@xapnghanaltd.com",
   location: "Oak House, Ablakwa Aidoo Street, East Legon Hills",
+  locationHref:
+    "https://www.google.com/maps/search/?api=1&query=Oak%20House%2C%20Ablakwa%20Aidoo%20Street%2C%20East%20Legon%20Hills%2C%20Ghana",
 };
